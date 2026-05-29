ed95265d0d0bc4ac0c96349d0f7dc20445dfa188
OlegKorniychuk <kornoleg576@gmail.com>
2026-05-29 23:48:35 +0300

    feat: set up log streaming to elasticsearch + kibana

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,25 @@
+import fs from 'fs';
 import pino from 'pino';
+import { ecsFormat } from '@elastic/ecs-pino-format';
 
-export const logger = pino({
-  level: process.env.NODE_ENV === 'test' ? 'silent' : 'info',
-});
+function buildStream() {
+  if (process.env.NODE_ENV === 'test') return undefined;
+
+  const streams: pino.StreamEntry[] = [{ stream: process.stdout }];
+
+  if (process.env.LOG_FILE) {
+    streams.push({
+      stream: fs.createWriteStream(process.env.LOG_FILE, { flags: 'a' }),
+    });
+  }
+
+  return streams.length > 1 ? pino.multistream(streams) : process.stdout;
+}
+
+export const logger = pino(
+  {
+    ...ecsFormat(),
+    level: process.env.NODE_ENV === 'test' ? 'silent' : 'info',
+  },
+  buildStream(),
+);
